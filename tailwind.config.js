a2b045f037fc29632b30d64f569c7d1fa5894939
Theopelu <theopelu5262@gmail.com>
2024-06-10 19:36:39 +0800

    web tailwindcss 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#7F56D9",
+      },
+    },
+  },
+  plugins: [],
+};
